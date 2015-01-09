bc6b94722a7bc095a7f7fde76192f3f6a4b06d2b
Medma Infomatix <medma@MedmaMahipal.local>
2015-01-09 10:44:46 +0530

    Initial Commit

---
+++ gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by Medma Infomatix on 09/01/15.
+//  Copyright (c) 2015 Medma Infomatix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitDemo
+//
+//  Created by Medma Infomatix on 09/01/15.
+//  Copyright (c) 2015 Medma Infomatix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
